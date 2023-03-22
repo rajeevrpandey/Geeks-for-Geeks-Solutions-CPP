ca9cd20d10288311d28aa329fc383d5181ef3f1f
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-22 10:32:28 +0530

    Create Palindrome String.cpp

---
+++ Easy/Palindrome String.cpp
@@ -0,0 +1,77 @@
+/*
+Given a string S, check if it is palindrome or not.
+
+Example 1:
+
+Input: S = "abba"
+Output: 1
+Explanation: S is a palindrome
+Example 2:
+
+Input: S = "abc" 
+Output: 0
+Explanation: S is not a palindrome
+ 
+
+Your Task:
+You don't need to read input or print anything. Complete the function isPalindrome()which accepts string S and returns an integer value 1 or 0.
+
+Expected Time Complexity: O(Length of S)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 <= Length of S<= 10^5
+*/
+
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes her
+	    auto it1 = S.begin();
+	    auto it2 = S.end();
+	    it2--;
+	    for(; distance(it1, it2)>=0; it1++,it2--){
+	        if(*it1 != *it2){
+	            return 0;
+	        }
+	    }
+	    return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends

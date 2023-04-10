9e0d8e25dd1d9b4cb197ce6bc1cab86348e12c82
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-04-10 10:34:56 +0530

    Create Insertion Sort Recursive.cpp

---
+++ Easy/Insertion Sort Recursive.cpp
@@ -0,0 +1,97 @@
+/*
+The task is to complete the insert() function which is used to implement Insertion Sort.
+
+
+Example 1:
+
+Input:
+N = 5
+arr[] = { 4, 1, 3, 9, 7}
+Output:
+1 3 4 7 9
+Example 2:
+
+Input:
+N = 10
+arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+Output:
+1 2 3 4 5 6 7 8 9 10
+
+Your Task: 
+You don't have to read input or print anything. Your task is to complete the function insert() and insertionSort() where insert() takes the array, it's size and an index i and insertionSort() uses insert function to sort the array in ascending order using insertion sort algorithm. 
+
+Expected Time Complexity: O(N*N).
+Expected Auxiliary Space: O(1).
+
+
+Constraints:
+1 <= N <= 1000
+1 <= arr[i] <= 1000
+*/
+
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void insert(int arr[], int i, int n)
+    {
+        //code here
+        if(i==n) return;
+        
+        for(int j=i; j>0; j--){
+            if(arr[j]<arr[j-1]){
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+        insert(arr, i+1, n);
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        
+        insert(arr, 0, n);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends

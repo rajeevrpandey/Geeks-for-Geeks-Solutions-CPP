06a837cb956d67df87a05b01e63c4cbbbf16082c
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-04-18 15:54:12 +0530

    Create Check if array is sorted.cpp

---
+++ Easy/Check if array is sorted.cpp
@@ -0,0 +1,75 @@
+/*
+Given an array arr[] of size N, check if it is sorted in non-decreasing order or not. 
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {10, 20, 30, 40, 50}
+Output: 1
+Explanation: The given array is sorted.
+Example 2:
+
+Input:
+N = 6
+arr[] = {90, 80, 100, 70, 40, 30}
+Output: 0
+Explanation: The given array is not sorted.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function arraySortedOrNot() which takes the arr[] and N as input parameters and returns a boolean value (true if it is sorted otherwise false).
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 10^5
+1 ≤ Arr[i] ≤ 10^6
+*/
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        // code here
+        int i = 0;
+        while(i<n-1){
+            if(arr[i+1]<arr[i]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        bool ans = ob.arraySortedOrNot(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

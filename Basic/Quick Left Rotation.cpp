081c3b451eb76fa9c8c6e88535c78d8cf2f82db7
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-04-21 14:52:09 +0530

    Create Quick Left Rotation.cpp

---
+++ Basic/Quick Left Rotation.cpp
@@ -0,0 +1,109 @@
+/*
+Given an array arr[] of size N and an integer K, the task is to left rotate the array K indexes
+
+Example 1:
+
+Input: N = 7, K = 2
+arr[] = {1, 2, 3, 4, 5, 6, 7}
+Output: 3 4 5 6 7 1 2
+Explanation: Rotation of the above 
+array by 2 will make the output array .
+
+Example 2:
+
+Input: N = 6, K = 12
+arr[] = {1, 2, 3, 4, 5, 6}
+Output: 1 2 3 4 5 6
+
+Your Task:
+This is a function problem. You don't need to take any input, as it is already accomplished by the driver code. You just need to complete the function leftRotate() that takes array arr, integer K and integer N as parameters and rotate the given array by d value.
+
+ 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+ 
+
+Constraints:
+1 ≤ N ≤ 10^5
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	void leftRotate(int arr[], int k, int n) 
+	{ 
+	   // Your code goes here
+	   k=k%n;
+
+        if(k<=n){
+            long temp[k];
+            
+            for(int i=0; i<k; i++){
+                temp[i] = arr[i];
+                
+            }
+        
+            for(int j=k; j<n;j++){
+            
+                arr[j-k] = arr[j];
+            
+            }
+        
+            for(int f=0; f<k; f++){
+            
+                arr[n-k+f] = temp[f];
+            
+            }
+        
+        }
+    }
+		 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+	    cin >> n;
+	    int k;
+	    cin >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	        // um[a[i]]++;
+	    }
+
+
+       
+
+        Solution ob;
+        ob.leftRotate(a,k,n);
+        
+        for (int i = 0; i < n; i++) 
+        	cout << a[i] << " "; 
+
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends

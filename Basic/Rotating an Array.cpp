6280d8b642cec50665e11b4b1464c279b18648f2
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-04-21 15:00:19 +0530

    Create Rotating an Array.cpp

---
+++ Basic/Rotating an Array.cpp
@@ -0,0 +1,91 @@
+/*
+Given an array of size N. The task is to rotate array by D elements where D ≤ N.
+
+Example 1:
+
+Input:
+N = 7
+Arr[] = {1, 2, 3, 4, 5, 6, 7}
+D = 2
+Output: 3 4 5 6 7 1 2
+Explanation: 
+Rotate by 1: [2, 3, 4, 5, 6, 7, 1]
+Rotate by 2: [3, 4, 5, 6, 7, 1, 2]
+
+Example 2:
+
+Input:
+N = 4
+Arr[] = {1, 3, 4, 2}
+D = 3
+Output: 2 1 3 4
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function leftRotate() which takes the array of integers arr[], its size n and d as input parameters and rotates arr[] in-place without using any extra memory.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 10^5
+1 ≤ Arr[i] ≤ 1000
+0 ≤ D ≤ N
+*/
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void leftRotate(int arr[], int n, int d) {
+        // code here
+        int temp[d];
+        for(int i=0; i<d; i++){
+            temp[i] = arr[i];
+        }
+        for(int i=d; i<n; i++){
+            arr[i-d] = arr[i];
+        }
+        int j = 0;
+        for(int i=n-d; i<n; i++){
+            
+            arr[i] = temp[j];
+            j++;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, d;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> d;
+        Solution ob;
+        ob.leftRotate(arr, n, d);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
+
